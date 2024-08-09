a50f87ba4ad81da01a839100a0617197a9764adf
renzozuk <renzo.zuk19@gmail.com>
2024-08-09 16:16:27 -0300

    Frontend: Sequence buttons are working correctly now

--- frontend/medal-table-app/src/App.jsx
+++ frontend/medal-table-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Line from "./components/Line.jsx";
 import SequenceButton from "./components/SequenceButton.jsx";
 import processData from "./util/data.js";
@@ -7,36 +7,42 @@ import "./App.css";
 export default function App() {
     const [countries, setCountries] = useState([]);
 
-    document.addEventListener("DOMContentLoaded", loadData("/gold"));
+    useEffect(() => {
+        loadData("/gold");
+    }, []);
 
-    function loadData(parameter) {
-        useEffect(() => {
-            processData(parameter).then((countriesData) => {
-                setCountries(countriesData);
-            });
+    const loadData = (parameter) => {
+        processData(parameter).then((countriesData) => {
+            setCountries(countriesData);
         });
-    }
+    };
 
     return (
         <div id="page">
             <div className="side-bar"></div>
             <div className="content">
-                <div class="header">
+                <div className="header">
                     <p className="header-title">Olympic Medal Table</p>
                 </div>
                 <div className="sub-header sequence-buttons">
-                    <SequenceButton image={false} content={"A-Z"} arrow={true} />
-                    <SequenceButton image={false} content={"A-Z"} arrow={false} />
-                    <SequenceButton image={true} content={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Gold_medal.svg/1024px-Gold_medal.svg.png"} arrow={true} />
-                    <SequenceButton image={true} content={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Gold_medal.svg/1024px-Gold_medal.svg.png"} arrow={false} />
-                    <SequenceButton image={true} content={"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/GoldSilverBronze_medals.svg/1920px-GoldSilverBronze_medals.svg.png"} arrow={true} />
-                    <SequenceButton image={true} content={"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/GoldSilverBronze_medals.svg/1920px-GoldSilverBronze_medals.svg.png"} arrow={false} />
+                    <SequenceButton image={false} content={"A-Z"} arrow={true} onClick={() => loadData("")} />
+                    <SequenceButton image={false} content={"A-Z"} arrow={false} onClick={() => loadData("/reverse")} />
+                    <SequenceButton image={true} content={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Gold_medal.svg/1024px-Gold_medal.svg.png"} arrow={true} onClick={() => loadData("/gold")} />
+                    <SequenceButton image={true} content={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Gold_medal.svg/1024px-Gold_medal.svg.png"} arrow={false} onClick={() => loadData("/gold/reverse")} />
+                    <SequenceButton image={true} content={"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/GoldSilverBronze_medals.svg/1920px-GoldSilverBronze_medals.svg.png"} arrow={true} onClick={() => loadData("/all-medals")} />
+                    <SequenceButton image={true} content={"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/GoldSilverBronze_medals.svg/1920px-GoldSilverBronze_medals.svg.png"} arrow={false} onClick={() => loadData("/all-medals/reverse")} />
                 </div>
                 <div className="sub-header sub-header-labels">
                     <div className="sub-header sub-header-start">
-                        <p className="sub-header-label" id="hashtag-label">#</p>
-                        <p className="sub-header-label" id="flag-label">Flag</p>
-                        <p className="sub-header-label" id="name-label">Name</p>
+                        <p className="sub-header-label" id="hashtag-label">
+                            #
+                        </p>
+                        <p className="sub-header-label" id="flag-label">
+                            Flag
+                        </p>
+                        <p className="sub-header-label" id="name-label">
+                            Name
+                        </p>
                     </div>
                     <div className="sub-header sub-header-end">
                         <img className="single-medal" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Gold_medal.svg/1024px-Gold_medal.svg.png"></img>
--- frontend/medal-table-app/src/components/SequenceButton.jsx
+++ frontend/medal-table-app/src/components/SequenceButton.jsx
@@ -2,7 +2,7 @@ import "./SequenceButton.css";
 
 export default function SequenceButton(props) {
     return(
-        <div className="button">
+        <button className="button" onClick={props.onClick}>
             <div className="left-side">
                 {props.image ? <img className="medal-button-image" src={props.content}></img> : <p className="button-text">{props.content}</p>}
             </div>
@@ -10,6 +10,6 @@ export default function SequenceButton(props) {
                 <img className="arrow-image" src={"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Increase2.svg/22px-Increase2.svg.png"}></img> : 
                 <img className="arrow-image" src={"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Decrease2.svg/22px-Decrease2.svg.png"}></img>}
             </div>   
-        </div>
+        </button>
     );
 }
